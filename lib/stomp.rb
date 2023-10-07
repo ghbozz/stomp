74394f845b9d275c0a4bfcc3016f99e6329ab572
Romain Sanson <romain.sanson@hey.com>
2023-10-07 21:08:39 +0200

    initial commit

---
+++ lib/stomp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stomp/version"
+
+module Stomp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/stomp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Stomp
+  VERSION = "0.1.0"
+end
