5a27045c448901767e04effcc95760213c2a4b33
Romain Sanson <romain.sanson@hey.com>
2023-10-14 18:33:54 +0200

    cleanup

--- lib/stomp.rb
+++ lib/stomp.rb
@@ -31,4 +31,16 @@ module Stomp
       @previous_step = step&.to_sym
     end
   end
+
+  module Controller
+    def build_record_for(klass)
+      record = klass.new(serialized_steps_data: params[:serialized_steps_data])
+      record.valid? if record.should_validate?
+      record
+    end
+
+    def next_step_path_for(record, options = {})
+      public_send("#{options[:path]}", serialized_steps_data: record.serialized_steps_data)
+    end
+  end
 end
